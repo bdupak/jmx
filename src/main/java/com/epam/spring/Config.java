c051eedc0a6861d164cc06d1fcca5859004515f0
Bohdan Dupak <bdupak@cartera.com>
2018-05-02 12:35:31 +0300

    added jmx spring

---
+++ src/main/java/com/epam/spring/Config.java
@@ -0,0 +1,52 @@
+package com.epam.spring;
+
+import org.springframework.jmx.export.annotation.ManagedAttribute;
+import org.springframework.jmx.export.annotation.ManagedOperation;
+import org.springframework.jmx.export.annotation.ManagedResource;
+
+@ManagedResource
+public class Config {
+
+    private String timeout;
+    private String poolSize;
+    private boolean stopApp;
+
+    @ManagedAttribute
+    public String getTimeout() {
+        return timeout;
+    }
+
+    @ManagedAttribute
+    public void setTimeout(String timeout) {
+        this.timeout = timeout;
+    }
+
+    @ManagedAttribute
+    public String getPoolSize() {
+        return poolSize;
+    }
+
+    @ManagedAttribute
+    public void setPoolSize(String poolSize) {
+        this.poolSize = poolSize;
+    }
+
+    @ManagedAttribute
+    public boolean isStopApp() {
+        return stopApp;
+    }
+
+    @ManagedAttribute
+    public void setStopApp(boolean stopApp) {
+        this.stopApp = stopApp;
+    }
+
+    @Override
+    @ManagedOperation
+    public String toString() {
+        return "Config{" +
+                "timeout='" + timeout + '\'' +
+                ", poolSize='" + poolSize + '\'' +
+                '}';
+    }
+}
